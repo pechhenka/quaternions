fa1d83b39df05b5fe00daba7f577aa127684547c
pechhenka <mail@pechhenka.ru>
2020-05-17 16:54:26 +0300

    example ready

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[ExecuteAlways]
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Параметры кватерниона")]
+    [Tooltip("Объект, который задаёт ось вращения")]public Transform Vector;
+    [Tooltip("Угол (в радианах)")] public float Angel;
 
-    // Update is called once per frame
+    [Header("Управляемый объект")]
+    [Tooltip("Объект, которому задаём вращение")] public GameObject RotationObject;
+
     void Update()
     {
+        Vector3 v = Vector.position - RotationObject.transform.position;
+
+        ShowDebug(v);
         
+        RotationObject.transform.rotation = GetQuaternion(v,Angel);
+    }
+
+    Quaternion GetQuaternion(in Vector3 vector, in float Angel)
+    {
+        Vector3 v = vector.normalized;
+        float t = Mathf.Sin(Angel / 2);
+        return new Quaternion(t * v.x, t * v.y, t * v.z, Mathf.Cos(Angel / 2));
+    }
+
+    void ShowDebug(in Vector3 v)
+    {
+        Debug.DrawLine(RotationObject.transform.position, Vector.position - RotationObject.transform.position); // ось
+        Debug.DrawLine(Vector3.zero, v.normalized, Color.red); // нормализация
     }
 }
